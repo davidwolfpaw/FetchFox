2ceb4aa2dac7e93b7c7d0de34e42434cc1d3b192
davidwolfpaw <david@davidwolfpaw.com>
2023-11-21 18:29:07 -0500

    fixes publishd date cell

--- popup/popup.js
+++ popup/popup.js
@@ -121,7 +121,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 row.insertCell().textContent = meta.title || 'No title';
                 row.insertCell().textContent = meta.url || 'No URL';
                 row.insertCell().textContent = meta.author || 'No author';
-                row.insertCell().textContent = formatDate(meta.publishDate) || 'No publish date';
+                row.insertCell().textContent = formatDate(meta.published) || 'No publish date';
 
                 // Set drag and drop attributes
                 row.setAttribute('draggable', true);
